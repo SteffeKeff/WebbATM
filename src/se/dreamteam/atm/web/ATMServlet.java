67033c87fd1b75e06d5bfa48ce570a36a882ae64
Stefan De Geer <sdgkeff@gmail.com>
2015-02-19 16:34:29 +0100

    klart

--- src/se/dreamteam/atm/web/ATMServlet.java
+++ src/se/dreamteam/atm/web/ATMServlet.java
@@ -1,90 +1,203 @@
 package se.dreamteam.atm.web;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.UUID;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import org.apache.catalina.ha.backend.Sender;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import se.dreamteam.atm.exception.ATMException;
 import se.dreamteam.atm.exception.ATMSecurityException;
 import se.dreamteam.atm.model.ATMCard;
 import se.dreamteam.atm.service.ATM;
 import se.dreamteam.atm.service.BankImpl;
+import sun.awt.RepaintArea;
+import sun.misc.UUDecoder;
 
-public final class ATMServlet extends HttpServlet {
+@WebServlet("/*")
+public final class ATMServlet extends HttpServlet
+{
 	private static final long serialVersionUID = 1L;
-	
+
 	ATM atm = new ATM();
-	
+	JSONObject jsonObject = null;
+
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html");
-		switch(request.getParameter("action")){
-			case "addbank": addBank(request.getParameter("bankid"), response); break;
-			case "addcustomer": addCustomer(request.getParameter("card"), request.getParameter("bank"), Integer.parseInt(request.getParameter("pin")), response); break;
-		}
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		response.setContentType("application/json");
+		ArrayList<HashMap> links = new ArrayList<>();
+		HashMap<String,String> link1 = new HashMap<>();
+		HashMap<String,String> link2 = new HashMap<>();
+		HashMap<String,String> link3 = new HashMap<>();
+		link1.put("href", "/banks");
+		link1.put("rel", "self");
+		link2.put("href", "/customer");
+		link2.put("rel", "self");
+		link3.put("href", "/login");
+		link3.put("rel", "self");
+		links.add(link1);
+		links.add(link2);
+		links.add(link3);
+		
+		JSONObject json = new JSONObject();
+		json.put("links", links);
+//		json.put("create bank", "/banks");
+//		json.put("create customer", "/customer");
+//		json.put("login", "/login");
+
+		response.getWriter().print(json);
 	}
-	
+
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
-		if("login".equals(request.getParameter("action"))){
-			response.setContentType("text/html");
-			login(request.getParameter("card"), request.getParameter("bank"), Integer.parseInt(request.getParameter("pin")), response, request);
-		}
-	}
-	
-	private void addBank(String bankId, HttpServletResponse response) throws IOException
-	{
-		if(!atm.hasBank(bankId))
+		//response.setContentType("application/json");
+		final String pathInfo = request.getPathInfo();
+		final String[] pathSegements = pathInfo.split("/");
+		
+
+		if (pathSegements.length == 2)
 		{
-			atm.addBank(new BankImpl(bankId)); 
-			response.getWriter().println("Bank added");
-			response.getWriter().println("<form action=\"/Webb-ATM\"><button type=\"submit\">back</button></form>");
-		}else{
-			response.sendError(400,"bank already exist");
-		}
-	}
-	
-	private void addCustomer(String accountHolderId, String bankId, int pin , HttpServletResponse response) throws IOException
-	{
-		if(atm.hasBank(bankId))
-		{	
-			try{
-				atm.addCard(new ATMCard(accountHolderId, bankId, pin));
-				response.getWriter().println("Card added");
-				response.getWriter().println("<form action=\"/Webb-ATM\"><button type=\"submit\">back</button></form>");
-			}catch(ATMException e){
+			final String path = pathSegements[1];
+			final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(request.getInputStream()));
+			JSONParser parser = new JSONParser();
+
+			try
+			{
+				jsonObject = (JSONObject) parser.parse(bufferedReader);
+			}
+			catch (ParseException e)
+			{
 				response.sendError(400, e.getMessage());
 			}
-			
-		}else{
+
+			switch (path)
+			{
+			case "banks":
+				addBank(getString("bankId"), response);
+				return;
+			case "customers":
+				addCustomer(getString("card"), getString("bankId"), Integer.parseInt(getString("pin")), response);
+				return;
+			case "login":
+				login(getString("card"), getString("bankId"), Integer.parseInt(getString("pin")), response, request);
+				return;
+			}
+		}
+		response.sendError(400, "bad request");
+	}
+	
+	private String getString(String value)
+	{
+		return (String) jsonObject.get(value);
+	}
+
+	private void addBank(String bankId, HttpServletResponse response) throws IOException
+	{
+		if (!atm.hasBank(bankId))
+		{
+			atm.addBank(new BankImpl(bankId));
+			response.setStatus(201);
+		}
+		else
+		{
+			response.sendError(400, "bank already exist");
+		}
+	}
+
+	private void addCustomer(String accountHolderId, String bankId, int pin, HttpServletResponse response) throws IOException
+	{
+		if (atm.hasBank(bankId))
+		{
+			try
+			{
+				atm.addCard(new ATMCard(accountHolderId, bankId, pin));
+				response.setStatus(201);
+			}
+			catch (ATMException e)
+			{
+				response.sendError(400, e.getMessage());
+			}
+
+		}
+		else
+		{
 			response.sendError(400, "bank doesnt exist");
 		}
 	}
-	
-	private void login(String card, String bank, int pin, HttpServletResponse response, HttpServletRequest request) throws IOException, ServletException
+
+	private void login(String cardHolderId, String bank, int pin, HttpServletResponse response, HttpServletRequest request) throws IOException, ServletException
 	{
 		HttpSession session = request.getSession();
-		try{
-			session.setAttribute("session", atm.verifyPin(pin, new ATMCard(card, bank, pin)));
-			session.setAttribute("name", card);
+		try
+		{
+			//session.setAttribute("session", atm.verifyPin(pin, new ATMCard(cardHolderId, bank, pin)));
+		
+			String id = UUID.randomUUID().toString();
+			session.setAttribute(id, atm.verifyPin(pin, new ATMCard(cardHolderId, bank, pin)));
+			session.setAttribute("id",id);
 			
-			RequestDispatcher rd = request.getRequestDispatcher("/ATMSession");
-			rd.forward(request, response);
+			response.setContentType("application/json");
+			JSONObject json = new JSONObject();
+			//json.put("id", id);
+			ArrayList<HashMap> links = new ArrayList<>();
+			HashMap<String,String> link1 = new HashMap<>();
+			HashMap<String,String> link2 = new HashMap<>();
+			HashMap<String,String> link3 = new HashMap<>();
+			HashMap<String,String> link4 = new HashMap<>();
+			link1.put("href", "/balance");
+			link1.put("rel", "self");
+			link2.put("href", "/deposit");
+			link2.put("rel", "self");
+			link3.put("href", "/withdraw");
+			link3.put("rel", "self");
+			link4.put("href", "/receipt");
+			link4.put("rel", "self");
+			links.add(link1);
+			links.add(link2);
+			links.add(link3);
+			links.add(link4);
 			
-		}catch(ATMSecurityException e)
+			json.put("links", links);
+//			JSONObject json = new JSONObject();
+//			json.put("links", links);
+//			
+//			json.put("check balance", "/balance");
+//			json.put("deposit", "/deposit");
+//			json.put("withdraw", "/withdraw");
+//			json.put("request receipt", "/receipt");
+			response.getWriter().print(json);
+			
+			//RequestDispatcher rd = request.getRequestDispatcher("/login/session");
+			//rd.forward(request, response);
+
+		}
+		catch (ATMSecurityException e)
 		{
 			response.sendError(401, e.getMessage());
-		}catch(ATMException e)
+		}
+		catch (ATMException e)
 		{
 			response.sendError(400, e.getMessage());
 		}
-		
+
 	}
 
 }
--- src/se/dreamteam/atm/web/ATMSession.java
+++ src/se/dreamteam/atm/web/ATMSession.java
@@ -1,103 +1,148 @@
 package se.dreamteam.atm.web;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 import se.dreamteam.atm.exception.ATMException;
 import se.dreamteam.atm.model.ATMReceipt;
 import se.dreamteam.atm.service.ATMSessionImpl;
 
-public final class ATMSession extends HttpServlet {
+@WebServlet("/session/*")
+public final class ATMSession extends HttpServlet
+{
 	private static final long serialVersionUID = 1L;
+	JSONObject jsonObject = null;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session = request.getSession();
-		ATMSessionImpl atmSession = (ATMSessionImpl) session.getAttribute("session");
-		if(!(atmSession == null)){
-			response.setContentType("text/html");
-			switch(request.getParameter("action")){
-				case "checkbalance": checkBalance(request, response, atmSession); break;
-				case "deposit": addAmount(request, response, atmSession); break;
-				case "withdraw": withdrawAmount(request, response, atmSession); break;
-				case "receipt": getReceipt(request, response, atmSession); break;
-			}
-			return;
-		}
-		response.sendError(401, "not authenticated");
-	}
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session = request.getSession();
-		ATMSessionImpl atmSession = (ATMSessionImpl) session.getAttribute("session");
-		if(atmSession != null){
-			response.setContentType("text/html");
-			if("login".equals(request.getParameter("action"))){
-				login(request, response, session);
-			}
-		}
-	}
-	
-	private void login(HttpServletRequest request, HttpServletResponse response, HttpSession session) throws IOException
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
-		if(session != null){
-			response.getWriter().println("Welcome  " + session.getAttribute("name") + "!");
-			response.getWriter().println("<form method=\"GET\" action=\"ATMSession\"><button type=\"submit\" name=\"action\" VALUE=\"checkbalance\" >check balance</button></form>");
-			response.getWriter().println("<form method=\"GET\" action=\"ATMSession\"><input type=\"text\" name=\"amount\" placeholder=\"amount\"/><button type=\"submit\" name=\"action\" VALUE=\"deposit\" >deposit</button></form>");
-			response.getWriter().println("<form method=\"GET\" action=\"ATMSession\"><input type=\"text\" name=\"amount\" placeholder=\"amount\"/><button type=\"submit\" name=\"action\" VALUE=\"withdraw\" >withdraw</button></form>");
-			response.getWriter().println("<form method=\"GET\" action=\"ATMSession\"><button type=\"submit\" name=\"action\" VALUE=\"receipt\" >get receipt</button></form>");
-			response.getWriter().println("<form action=\"/Webb-ATM\"><button type=\"submit\">Log out</button></form>");
-			return;
-		}
-		response.sendError(401, "Your not allowed to access this content");
+
 	}
-	
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		HttpSession session = request.getSession();
+		String id = (String) session.getAttribute("id");
+		ATMSessionImpl atmSession = (ATMSessionImpl) session.getAttribute(id);
+
+		final String pathInfo = request.getPathInfo();
+		final String[] pathSegements = pathInfo.split("/");
+
+		if (pathSegements.length == 2)
+		{
+			final String path = pathSegements[1];
+
+			if (atmSession != null)
+			{
+				response.setContentType("application/json");
+				switch (path)
+				{
+				case "balance":
+					checkBalance(request, response, atmSession);
+					return;
+				case "deposit":
+					deposit(request, response, atmSession);
+					return;
+				case "withdraw":
+					withdraw(request, response, atmSession);
+					return;
+				case "receipt":
+					getReceipt(request, response, atmSession);
+					return;
+				}
+			}else{
+				response.sendError(401, "not authenticated");
+			}
+		}
+		response.sendError(400, "bad request");
+	}
+
 	private void checkBalance(HttpServletRequest request, HttpServletResponse response, ATMSessionImpl atmSession) throws IOException
 	{
-		try{
-			response.getWriter().println("Current Balance: " + atmSession.checkBalance());
-			response.getWriter().println("<form method=\"POST\" action=\"ATMSession\"><button type=\"submit\" name=\"action\" VALUE=\"login\">back</button></form>");
-		}catch(ATMException e){
+		try
+		{
+			JSONObject json = new JSONObject();
+			json.put("balance", atmSession.checkBalance());
+			response.getWriter().print(json);
+		}
+		catch (ATMException e)
+		{
 			response.sendError(400, e.getMessage());
 		}
 	}
 
-	private void addAmount(HttpServletRequest request, HttpServletResponse response, ATMSessionImpl atmSession) throws IOException
+	private void deposit(HttpServletRequest request, HttpServletResponse response, ATMSessionImpl atmSession) throws IOException
 	{
-		atmSession.deposit(Integer.parseInt(request.getParameter("amount")));
-		response.getWriter().println(request.getParameter("amount") + " is deposited");
-		response.getWriter().println("<form method=\"POST\" action=\"ATMSession\"><button type=\"submit\" name=\"action\" VALUE=\"login\">back</button></form>");
+		setJsonObject(request,response);
+		atmSession.deposit(Integer.parseInt(getString("amount",response)));
+		response.getWriter().println(jsonObject);
 	}
 
-	private void withdrawAmount(HttpServletRequest request, HttpServletResponse response, ATMSessionImpl atmSession) throws IOException
+	private void withdraw(HttpServletRequest request, HttpServletResponse response, ATMSessionImpl atmSession) throws IOException
 	{
-		try{
-			atmSession.withdrawAmount(Integer.parseInt(request.getParameter("amount")));
-			response.getWriter().println("You have withdrawn: " +request.getParameter("amount"));
-			response.getWriter().println("<form method=\"POST\" action=\"ATMSession\"><button type=\"submit\" name=\"action\" VALUE=\"login\">back</button></form>");;
-		}catch(ATMException e){
+		setJsonObject(request,response);
+		try
+		{
+			atmSession.withdrawAmount(Integer.parseInt(getString("amount", response)));
+			response.getWriter().println(jsonObject);
+		}
+		catch (ATMException e)
+		{
 			response.sendError(400, e.getMessage());
 		}
-		
+
 	}
-	
+
 	private void getReceipt(HttpServletRequest request, HttpServletResponse response, ATMSessionImpl atmSession) throws IOException
 	{
-		try{
+		try
+		{
 			ATMReceipt receipt = atmSession.requestReceipt(atmSession.getTransactionId());
-			response.getWriter().println("Receipt" );
-			response.getWriter().println("<br>Date: " + receipt.getDate());
-			response.getWriter().println("<br>Amount: " + receipt.getAmount() );
-			response.getWriter().println("<br>Id: " + receipt.getTransactionId());
 			
-			response.getWriter().println("<form method=\"POST\" action=\"ATMSession\"><button type=\"submit\" name=\"action\" VALUE=\"login\">back</button></form>");
-		}catch(ATMException e){
+			JSONObject json = new JSONObject();
+			json.put("transactionId", receipt.getTransactionId());
+			json.put("date", receipt.getDate());
+			json.put("amount", receipt.getAmount());
+			response.getWriter().print(json);
+			
+		}catch (ATMException e)
+		{
 			response.sendError(400, e.getMessage());
 		}
 	}
-
+	
+	private void setJsonObject(HttpServletRequest request, HttpServletResponse response) throws IOException
+	{
+		final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(request.getInputStream()));
+		JSONParser parser = new JSONParser();
+		try
+		{
+			jsonObject = (JSONObject) parser.parse(bufferedReader);
+		}
+		catch (ParseException e)
+		{
+			response.sendError(400, e.getMessage());
+		}
+	}	
+	
+	private String getString(String value, HttpServletResponse response) throws IOException
+	{
+		try{
+			return (String) jsonObject.get(value);
+		}catch(NullPointerException e){
+			response.sendError(400,"no amount...");
+		}
+		return null;
+	}
 }
